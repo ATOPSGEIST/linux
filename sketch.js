80b1f80b44c2eabf957a51e3dd3b8f65801e6f8b
ATOPSGEIST <150094064+ATOPSGEIST@users.noreply.github.com>
2023-11-09 01:44:56 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,9 @@ let img2;
 let img3;
 let imgs = ['img', 'img1', 'img2', 'img3'];
 let currentTexture; // Variable to store the current texture
+let alphaValue = 0; // Start with an invisible image (alpha = 0)
+let fadeInTime = 600000; // Fade in time in milliseconds (2 minutes)
+let lastUpdateTime = 0; // Last time the alpha was updated
 
 function preload(){
   img = loadImage('cb86ca7f-2c05-4496-85a8-eee92c600307.jpg')
@@ -23,6 +26,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   // Initialize the current texture with the first image
   currentTexture = img;
+  lastUpdateTime = millis();
 }
 
 function getRandomImage() {
@@ -34,6 +38,7 @@ function getRandomImage() {
 }
 
 function draw() {
+  let elapsedTime = millis() - lastUpdateTime;
   // Update the position of the ellipse
   x += T;
   y += h;
@@ -44,7 +49,7 @@ function draw() {
   pointLight(250, 200, 180, -2000, -2000, 400);
 
   
-
+push()
   // Change the color of the ellipse when it hits the edge
   if (x <= 150 || x >= windowWidth - 150 || y <= 150 || y >= windowHeight - 150) {
     currentTexture = getRandomImage();
@@ -69,4 +74,22 @@ function draw() {
   if (x >= windowWidth - 150 || x <= 150) {
     T = -T;
   }
+  if (elapsedTime > fadeInTime) {
+    alphaValue = 255; // Ensure we don't exceed the maximum alpha value
+  } else {
+    // Map the elapsed time to the alpha range (0 - 255)
+    alphaValue = map(elapsedTime, 0, fadeInTime, 0, 255);
+  }
+pop()
+
+  tint(255, alphaValue); // Apply the alpha value to the image
+  image(img, -1*windowWidth/2, -1*windowHeight/2, windowWidth, windowHeight); // Draw the image centered on the canvas
+   if (elapsedTime > fadeInTime) {
+    alphaValue = 2; // Ensure we don't exceed the maximum alpha value
+  } else {
+    // Map the elapsed time to the alpha range (0 - 255)
+    alphaValue = map(elapsedTime, 0, fadeInTime, 0, 255);
+  }
+
 }
+
